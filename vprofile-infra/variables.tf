44309d82fb3bf8775dca4f6507b55dd687d39f69
KenzyTran <trantankiet140401@gmail.com>
2026-06-02 18:22:38 +0700

    Update Kubernetes version to 1.35 for EKS cluster

--- vprofile-infra/variables.tf
+++ vprofile-infra/variables.tf
@@ -13,7 +13,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "1.31"
+  default     = "1.35"
 }
 
 variable "vpc_cidr" {
